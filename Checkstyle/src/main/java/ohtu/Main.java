ea106da3ccb3432939428d73f1d7d899ab9a1a42
aorin <aorin@users.noreply.github.com>
2016-04-10 19:42:16 +0300

    Lisätty checkstyle säännöt

--- Checkstyle/src/main/java/ohtu/Main.java
+++ Checkstyle/src/main/java/ohtu/Main.java
@@ -10,26 +10,29 @@ public class Main {
         ApplicationContext ctx = new FileSystemXmlApplicationContext("src/main/resources/spring-context.xml");
    
         Kauppa kauppa = ctx.getBean(Kauppa.class);
+	kauppaAsiointi1(kauppa);
+	kauppaAsiointi2(kauppa);
 
-        // kauppa hoitaa yhden asiakkaan kerrallaan seuraavaan tapaan:
+        // kirjanpito
+        for (String tapahtuma : ctx.getBean(Kirjanpito.class).getTapahtumat()) {
+            System.out.println(tapahtuma);
+        }
+    }
+
+    private static void kauppaAsiointi1(Kauppa kauppa) {
         kauppa.aloitaAsiointi();
         kauppa.lisaaKoriin(1);
         kauppa.lisaaKoriin(3);
         kauppa.lisaaKoriin(3);
         kauppa.poistaKorista(1);
         kauppa.tilimaksu("Pekka Mikkola", "1234-12345");
+    }
 
-        // seuraava asiakas
-        kauppa.aloitaAsiointi();
+    private static void kauppaAsiointi2(Kauppa kauppa) {
+	kauppa.aloitaAsiointi();
         for (int i = 0; i < 24; i++) {
             kauppa.lisaaKoriin(5);
         }
-
         kauppa.tilimaksu("Arto Vihavainen", "3425-1652");
-
-        // kirjanpito
-        for (String tapahtuma : ctx.getBean(Kirjanpito.class).getTapahtumat()) {
-            System.out.println(tapahtuma);
-        }
     }
 }
